8ce5562f3ddcee54bc9ffedd99405190cd93bce2
TrdHuy <trdtranduchuy@users.noreply.github.com>
2020-10-27 00:38:25 +0700

    Initial commit
    Signed-off-by: TrdHuy <trdtranduchuy@users.noreply.github.com>

---
+++ Pharmacy/Pharmacy/Base/Observable/OnPropertyChanged.cs
@@ -0,0 +1,29 @@
+﻿using Pharmacy.Base.Observable.ObserverPattern;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Base.Observable
+{
+    abstract class OnPropertyChanged : BaseObservable, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onChanged(object viewModel, string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            PropertyChanged?.Invoke(viewModel, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [Conditional("DEBUG")]
+        private void VerifyPropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+                throw new ArgumentNullException(GetType().Name + " does not contain property: " + propertyName);
+        }
+    }
+}
---
+++ Pharmacy/Pharmacy/Implement/Windows/LoginScreenWindow/MVVM/ViewModels/LoginScreenWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Pharmacy.Implement.Windows.LoginScreenWindow.Core.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Implement.Windows.LoginScreenWindow.MVVM.ViewModels
+{
+    class LoginScreenWindowViewModel : BaseLoginScreenWindowViewModel
+    {
+        public LoginScreenWindowViewModel()
+        {
+
+        }
+
+        public override void Update()
+        {
+        }
+
+        protected override void InitPropertiesRegistry()
+        {
+
+        }
+    }
+}
