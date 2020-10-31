823bcf28a4b58ccd72bd875f14259f4d85454d04
TrdHuy <trdtranduchuy@users.noreply.github.com>
2020-11-01 02:53:36 +0700

    Implement Base model and Data base
    Add AbstractBaseModel, UserModel
    Add PharmacyDB

---
+++ Pharmacy/Pharmacy/Base/MVVM/Model/AbstractBaseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Base.MVVM.Model
+{
+    abstract class AbstractBaseModel
+    {
+        private Dictionary<string, object> _parts = new Dictionary<string, object>();
+
+        public object this[string key]
+        {
+            get
+            {
+                try
+                {
+                    return _parts[key];
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+
+                ;
+            }
+
+            set { _parts[key] = value; }
+        }
+
+    }
+}
--- Pharmacy/Pharmacy/Implement/Windows/LoginScreenWindow/Action/Type/LSW_SystemLoginAction.cs
+++ Pharmacy/Pharmacy/Implement/Windows/LoginScreenWindow/Action/Type/LSW_SystemLoginAction.cs
@@ -1,17 +1,56 @@
 ﻿using Pharmacy.Base.UIEventHandler.Action;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Pharmacy.Implement.Windows.LoginScreenWindow.Action.Type
 {
     class LSW_SystemLoginAction : IAction
     {
+        private const string DataConectionPath = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\PharmacyDB.mdf;Integrated Security=True";
+
         public bool Execute(object[] dataTransfer)
         {
-            throw new NotImplementedException();
+            string userName = "huyTran";
+            string passWord = "123456";
+
+            SqlConnection sqlCon = new SqlConnection(DataConectionPath);
+            try
+            {
+                if(sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                String querry = "SELECT COUNT(1) FROM tblUser WHERE UserName=@Username AND UserPassword=@Password";
+                SqlCommand sqlCommand = new SqlCommand(querry, sqlCon);
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddWithValue("@Username", userName);
+                sqlCommand.Parameters.AddWithValue("@Password", passWord);
+
+                int count = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                if(count == 1)
+                {
+                    MessageBox.Show("Login Success!");
+                }
+                else
+                {
+                    MessageBox.Show("Login fail");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+            return true;
         }
     }
 }
