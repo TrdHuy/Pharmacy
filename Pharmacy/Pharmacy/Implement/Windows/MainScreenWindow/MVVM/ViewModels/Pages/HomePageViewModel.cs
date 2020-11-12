61e5d6980756ea2fa2ddae369d9fca69443a083e
TrdHuy <trdtranduchuy@users.noreply.github.com>
2020-11-12 14:03:31 +0700

    Implement Homepage UI
    Add new VietNamese fonts, and some icons
    Impl code follow UI (HompageUI.pptx)
    
    Signed-off-by: TrdHuy <trdtranduchuy@users.noreply.github.com>

---
+++ Pharmacy/Pharmacy/Implement/Windows/MainScreenWindow/MVVM/ViewModels/Pages/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Implement.Windows.MainScreenWindow.MVVM.ViewModels.Pages
+{
+    class HomePageViewModel
+    {
+    }
+}
