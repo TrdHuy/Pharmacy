9ee8f3487f9527ccceda522f3bbce360e14669d3
TrdHuy <trdtranduchuy@users.noreply.github.com>
2020-12-22 15:52:57 +0700

    [FRAMEWORK] Add string to image source converter

---
+++ Pharmacy/Pharmacy/Implement/Utils/Converter/StringToImageSourceConverter.cs
@@ -0,0 +1,29 @@
+﻿using Pharmacy.Base.Converter;
+using Pharmacy.Implement.Utils.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Implement.Utils.Converter
+{
+    public class StringToImageSourceConverter : BaseValueConverter<StringToImageSourceConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imageName = value.ToString();
+            string folderName = parameter.ToString();
+
+            return FileIOUtil.
+                GetBitmapFromName(imageName, folderName).
+                ToImageSource();
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Pharmacy/Pharmacy/Properties/Resources.Designer.cs
+++ Pharmacy/Pharmacy/Properties/Resources.Designer.cs
@@ -99,6 +99,16 @@ namespace Pharmacy.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap default_medicine_image {
+            get {
+                object obj = ResourceManager.GetObject("default_medicine_image", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap default_user_image {
             get {
                 object obj = ResourceManager.GetObject("default_user_image", resourceCulture);
