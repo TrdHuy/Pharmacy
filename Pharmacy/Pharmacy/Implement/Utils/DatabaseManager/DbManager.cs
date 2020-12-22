79b886b37f11195dcb937f4f2ebaebe44728deb6
TrdHuy <trdtranduchuy@users.noreply.github.com>
2020-12-22 10:57:20 +0700

    [Issue] The callback method was called duplicatly
    C: Provider did not un-subcribe observer due to boolean Updated.
    M: change Updated to true when the callback method was executed.

--- Pharmacy/Pharmacy/Implement/Utils/DatabaseManager/DbManager.cs
+++ Pharmacy/Pharmacy/Implement/Utils/DatabaseManager/DbManager.cs
@@ -25,10 +25,13 @@ namespace Pharmacy.Implement.Utils.DatabaseManager
             await Task.Delay(delayTime);
 
             _provider.ExecuteQueryAsync(cmdKey, paramaters);
+
+            // Issue: provider must un-subciribe observer coz it will callback next time
             if (observer.Updated)
             {
                 _provider.Unsubcribe(observer);
             }
+
         }
 
         public void RollBack()
