89fc7ba021fe4f9ab3cc515d73cb92e1d6bc2493
PhuocTran95 <nguyenbaphuoctran@gmail.com>
2021-01-18 22:58:18 +0700

    Update DB
    Works:
    - Add Medicine info to tblOrderDetail
    
    Signed-off-by: PhuocTran95 <nguyenbaphuoctran@gmail.com>
    Change-Id: Id552c13b46886218f8db012010d4f9fe013b2749

--- Pharmacy/Pharmacy/tblOrderDetail.cs
+++ Pharmacy/Pharmacy/tblOrderDetail.cs
@@ -15,12 +15,14 @@ namespace Pharmacy
     public partial class tblOrderDetail
     {
         public long OrderDetailID { get; set; }
+        public string MedicineID { get; set; }
         public double Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal TotalPrice { get; set; }
         public long OrderID { get; set; }
         public bool IsActive { get; set; }
     
+        public virtual tblMedicine tblMedicine { get; set; }
         public virtual tblOrder tblOrder { get; set; }
     }
 }
