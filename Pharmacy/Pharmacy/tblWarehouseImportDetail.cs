c875e8ad8771b343ceac80bfd506fcb43d9a13a1
TrdHuy <trdtranduchuy@users.noreply.github.com>
2021-01-18 13:37:52 +0700

    [FRAMEWORK] Update HPSolutionCCDev package ver 1.0.20, Update DB
    Remove foreign ImportDetailID key of tblOderDetail
    
    Change-Id: I1072a54f3856d19bc205a05a344779a371743ff2

--- Pharmacy/Pharmacy/tblWarehouseImportDetail.cs
+++ Pharmacy/Pharmacy/tblWarehouseImportDetail.cs
@@ -14,12 +14,6 @@ namespace Pharmacy
     
     public partial class tblWarehouseImportDetail
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public tblWarehouseImportDetail()
-        {
-            this.tblOrderDetails = new HashSet<tblOrderDetail>();
-        }
-    
         public long ImportDetailID { get; set; }
         public string MedicineID { get; set; }
         public double Quantity { get; set; }
@@ -28,8 +22,6 @@ namespace Pharmacy
         public bool IsActive { get; set; }
     
         public virtual tblMedicine tblMedicine { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<tblOrderDetail> tblOrderDetails { get; set; }
         public virtual tblWarehouseImport tblWarehouseImport { get; set; }
     }
 }
