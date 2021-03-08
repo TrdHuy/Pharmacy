e0d966173708ce882d3def0e49c75d4aa50e07eb
TrdHuy <trdtranduchuy@users.noreply.github.com>
2021-03-08 11:18:59 +0700

    [AppFeature] Implement ReportPage #1
    # Implement report page UI
    # Implement Statistic button for Revenue
    # Implement AsyncAction (use for some case need to async caculate, avoid blocking UI, applied to LoadChart method)
    # Update HPSCC Dev package ver 1.1.1 (new control MontuBox)

---
+++ Pharmacy/Pharmacy/Implement/Windows/MainScreenWindow/MVVM/ViewModels/Pages/ReportPage/OVs/MSW_RP_EventCommandOV.cs
@@ -0,0 +1,29 @@
+﻿using Pharmacy.Base.MVVM.ViewModels;
+using Pharmacy.Implement.Utils.InputCommand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace Pharmacy.Implement.Windows.MainScreenWindow.MVVM.ViewModels.Pages.ReportPage.OVs
+{
+    public class MSW_RP_EventCommandOV : BaseViewModel
+    {
+        public EventHandleCommand ReportPageLoaded { get; set; }
+        public MSW_RP_EventCommandOV(BaseViewModel parentModel) : base(parentModel)
+        {
+            ReportPageLoaded = new EventHandleCommand(OnReportPageLoaded);
+        }
+
+        private void OnReportPageLoaded(object sender, EventArgs e, object paramater)
+        {
+            var vm = ParentsModel as ReportPageViewModel;
+            if (vm != null)
+            {
+                vm.Chart = ((object[])paramater)[0] as BType_Chart;
+            }
+        }
+    }
+}
